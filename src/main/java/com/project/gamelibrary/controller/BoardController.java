9f01802d99288ac0ecc3dd77bdef9a0318084f48
JongminKim <whdals9265@naver.com>
2023-04-05 00:31:12 +0900

    파일전송..

--- src/main/java/com/project/gamelibrary/controller/BoardController.java
+++ src/main/java/com/project/gamelibrary/controller/BoardController.java
@@ -29,18 +29,13 @@ import org.springframework.web.multipart.MultipartRequest;
 import javax.imageio.ImageIO;
 import javax.validation.Valid;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -104,22 +99,81 @@ public class BoardController {
         return "/boards/boarddetails";
     }
     @RequestMapping(value="/singleImageUploader")
-    public void smarteditorImageUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String sFileInfo = "";
-        String sFilename = request.getHeader("file-name");
-        String sFilenameExt = sFilename.substring(sFilename.lastIndexOf(".")+1);
-        sFilenameExt = sFilenameExt.toLowerCase();
-        ServletInputStream inputStream = request.getInputStream();
-        byte[] bytes = new byte[1024];
-        System.out.println("inputStream 확인 :" + sFilename);
-        System.out.println("inputStream 확인 :" + inputStream);
+    public void smarteditorImageUpload(@RequestParam("file") List<MultipartFile> imgs, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            //파일정보
+            String sFileInfo = "";
+            //파일명을 받는다 - 일반 원본파일명
+            System.out.println(request.getPart("file"));
+            String filename = request.getHeader("file-name");
+            //파일 확장자
+            String filename_ext = filename.substring(filename.lastIndexOf(".") + 1);
+            //확장자를소문자로 변경
+            filename_ext = filename_ext.toLowerCase();
 
-        Files.copy(inputStream, Paths.get("/src/resources/img/"));
-        inputStream.close();
+            //이미지 검증 배열변수
+            String[] allow_file = {"jpg", "png", "bmp", "gif"};
 
+            //돌리면서 확장자가 이미지인지
+            int cnt = 0;
+            for (int i = 0; i < allow_file.length; i++) {
+                if (filename_ext.equals(allow_file[i])) {
+                    cnt++;
+                }
+            }
 
+            //이미지가 아님
+            if (cnt == 0) {
+                PrintWriter print = response.getWriter();
+                print.print("NOTALLOW_" + filename);
+                print.flush();
+                print.close();
+            } else {
+                //이미지이므로 신규 파일로 디렉토리 설정 및 업로드
+                //파일 기본경로
+                String dftFilePath = request.getSession().getServletContext().getRealPath("/");
+                //파일 기본경로 _ 상세경로
+                String filePath = dftFilePath + "resources" + File.separator + "editor" + File.separator + "multiupload" + File.separator;
+                System.out.println("filePath 경로 : " + filePath);
+                File file = new File(filePath);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                String realFileNm = "";
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+                String today = formatter.format(new java.util.Date());
+                realFileNm = today + UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf("."));
+                String rlFileNm = filePath + realFileNm;
+                ///////////////// 서버에 파일쓰기 /////////////////
+                InputStream is = request.getInputStream();
+                OutputStream os = new FileOutputStream(rlFileNm);
+                int numRead;
+                byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+                while ((numRead = is.read(b, 0, b.length)) != -1) {
+                    os.write(b, 0, numRead);
+                }
+                if (is != null) {
+                    System.out.println("inputStream 없음 ;");
+                    is.close();
+                }
+                os.flush();
+                os.close();
+                ///////////////// 서버에 파일쓰기 /////////////////
 
-        String[] whiteChk = {"jpg","png","bmp","gif"};
+                // 정보 출력
+                sFileInfo += "&bNewLine=true";
+                // img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함
+                sFileInfo += "&sFileName=" + filename;
+                ;
+                sFileInfo += "&sFileURL=" + "/resources/editor/multiupload/" + realFileNm;
+                PrintWriter print = response.getWriter();
+                print.print(sFileInfo);
+                print.flush();
+                print.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/fileDownload/{id}")
